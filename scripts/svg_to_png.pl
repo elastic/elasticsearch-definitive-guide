2895fc6fd74d5effe83b374b3ae4a5f3fe84085a
Clinton Gormley <clint@traveljury.com>
2014-03-07 13:36:24 +0100

    Added updated diagrams, plus script to resize pngs

---
+++ scripts/svg_to_png.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Path::Class;
+use Capture::Tiny qw(capture);
+my $inkscape = '/Applications/Inkscape.app/Contents/Resources/bin/inkscape';
+
+my $width = shift @ARGV or die "USAGE: $0 [width]\n";
+
+my $src = dir('svg');
+my $out = dir('png');
+
+$out->rmtree;
+$out->mkpath;
+
+while ( my $file = $src->next ) {
+    next unless -f $file;
+    my $name = $file->basename;
+    if ( $name =~ s/\.svg$// ) {
+        print "$name.svg -> $name.png\n";
+        my ( $stdout, $stderr, $exit ) = capture {
+            system( $inkscape, '--export-png', $out->file("$name.png"),
+                '-w', $width, $file );
+        };
+        if ($exit) {
+            die "Couldn't convert $file: $stderr\n";
+        }
+    }
+    else {
+        print "Skipping: $name\n";
+    }
+}
